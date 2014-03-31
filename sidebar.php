945f80413034be873eaea012d91b8b5d1e91c7e8
Amanda Rahra <amanda@canhoto.(none)>
2014-03-31 19:58:27 -0300

    small fix e mapa do google

--- sidebar.php
+++ sidebar.php
@@ -24,23 +24,28 @@
 	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 	<div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
 
+	<?php if (is_home()) { ?>
 	<div class="contato">
 		<div class="contato-telefone">
-			<div class="contato-texto"><b>telefone</b><br>+55 11 3296-7000</div>
 			<div class="contato-icone"></div>
+			<div class="contato-texto"><b>telefone</b><br>+55 11 3296-7000</div>
 		</div>
 		<div class="contato-email">
-			<div class="contato-texto"><b>email</b><br>contato@saopaulo.goethe.org</div>
 			<div class="contato-icone"></div>
+			<div class="contato-texto"><b>email</b><br>contato@saopaulo.goethe.org</div>
 		</div>
 		<div class="contato-local">
-			<div class="contato-texto"><b>local</b><br>Rua Lisboa, 974 - Pinheiros, SP</div>
 			<div class="contato-icone"></div>
+			<div class="contato-texto"><b>local</b><br>Rua Lisboa, 974 - Pinheiros, SP</div>
 		</div>
 
+		<div class="contato-mapa">
+		<iframe width="275" height="150" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=pt-BR&amp;geocode=&amp;q=rua+lisboa,+974&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=39.86519,86.572266&amp;ie=UTF8&amp;hq=rua+lisboa,+974&amp;hnear=&amp;radius=15000&amp;ll=-23.556072,-46.681872&amp;spn=0.071946,0.071946&amp;t=m&amp;output=embed"></iframe>
+		</div>
 	</div>
-
+	<?php } else { ?>
 		<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	<?php }	?>
 	</div><!-- #primary-sidebar -->
 	<?php endif; ?>
 </div><!-- #secondary -->
